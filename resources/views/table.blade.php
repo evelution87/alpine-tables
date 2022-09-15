4e703610317112c1bc65a1bbc7ef108c20db4c1e
Craig <craig@evelution.net>
2022-09-15 15:09:13 +1000

    Add PageString to top of list

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -5,13 +5,16 @@
 <div x-data="alpinetable(data)">
 	
 	<div class="flex justify-end items-center space-x-4 mb-3 text-gray-700">
-		<div x-show="loading" class="flex items-center">
-			<svg class="animate-spin mr-2 h-5 w-5 text-lime-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-				<path class="opacity-75" fill="currentColor"
-				      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-			</svg>
-			<span>Loading</span>
+		<div>
+			<div x-show="loading" class="flex items-center">
+				<svg class="animate-spin mr-2 h-5 w-5 text-lime-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+					<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+					<path class="opacity-75" fill="currentColor"
+					      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+				</svg>
+				<span>Loading</span>
+			</div>
+			<div x-show="!loading" x-text="pageString()" class="text-sm"></div>
 		</div>
 		<div x-show="show_search || filters.search.length" x-transition.opacity>
 			<label for="search">Search: </label>
