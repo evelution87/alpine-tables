a9ad9eb531a4132bcaa93c9891db36be114a6bec
Craig <craig@evelution.net>
2022-09-15 15:13:58 +1000

    Add PageString to top of list

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -5,7 +5,7 @@
 <div x-data="alpinetable(data)">
 	
 	<div class="flex justify-end items-center space-x-4 mb-3 text-gray-700">
-		<div>
+		<div class="flex-grow">
 			<div x-show="loading" class="flex items-center">
 				<svg class="animate-spin mr-2 h-5 w-5 text-lime-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
 					<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
